a0be12d9093332876a8300e8a1f4b61f65e7e4b3
João Paulo Araújo <jpf.araujo99@hotmail.com>
2024-09-07 17:17:45 -0300

    feat: creating style of the to do list in styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as c from './App.styles';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <c.Container>
+      <c.Area>
+        <c.Header>
+          To Do List
+        </c.Header>
+      </c.Area>
+    </c.Container>
   );
 }
 
