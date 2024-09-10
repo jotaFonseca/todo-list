2b5b19972ca03f835054c886c2ccb293f8922e81
João Paulo Araújo <jpf.araujo99@hotmail.com>
2024-09-10 17:52:45 -0300

    commit in other pc

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'react';
+import * as c from './App.styles';
+import { Item } from './types/item'
+
+const App = () => {
+  const [list, setList] = useState<Item[]>([
+    { id: 1, name: 'Comprar pão', done: false},
+    { id: 2, name: 'Comprar mortadela', done: false},
+  ])
+
+  return (
+    <c.Container>
+      <c.Area>
+        <c.Header>To Do List</c.Header>
+
+
+        {list.map((item, index) =>(
+          <div>{item.name}</div>
+        ))}
+
+      </c.Area>
+    </c.Container>
+  );
+}
+
+export default App;
